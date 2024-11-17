cfdba0d46ab3d35b884e12341bde038ebd99da02
eunice-hong <dev@eunice-hong.com>
2024-11-17 19:23:13 +0900

    test: add test for NoteRepository.createNote

---
+++ core/data/src/test/java/com/eunicehong/template/core/data/repository/note/EuniceRemoteClientFake.kt
@@ -0,0 +1,38 @@
+package com.eunicehong.template.core.data.repository.note
+
+import com.eunicehong.template.core.model.note.Note
+import com.eunicehong.template.core.remote.client.EuniceRemoteClient
+
+class EuniceRemoteClientFake : EuniceRemoteClient {
+    private val noteList =
+        mutableListOf(
+            Note(
+                id = 1,
+                userName = "eunice-hong",
+                content = "안녕하세요. 홍은이입니다.",
+                createdAt = 1632960000000,
+            ),
+            Note(
+                id = 2,
+                userName = "eunice-hong",
+                content = "방명록을 남겨주세요.",
+                createdAt = 1632960000000,
+            ),
+        )
+
+    override suspend fun getNoteList(): List<Note> = noteList
+
+    override suspend fun createNote(
+        userName: String,
+        content: String,
+    ) {
+        noteList.add(
+            Note(
+                id = noteList.size + 1L,
+                userName = userName,
+                content = content,
+                createdAt = System.currentTimeMillis(),
+            ),
+        )
+    }
+}
--- core/data/src/test/java/com/eunicehong/template/core/data/repository/note/NoteRepositoryUnitTest.kt
+++ core/data/src/test/java/com/eunicehong/template/core/data/repository/note/NoteRepositoryUnitTest.kt
@@ -1,5 +1,7 @@
 package com.eunicehong.template.core.data.repository.note
 
+import com.eunicehong.template.core.remote.client.EuniceRemoteClient
+import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
@@ -8,17 +10,33 @@ import org.junit.Test
  * 방명록 관련 데이터 처리 테스트
  */
 class NoteRepositoryUnitTest {
+    private val euniceRemoteClient: EuniceRemoteClient by lazy { EuniceRemoteClientFake() }
+
     private lateinit var noteRepository: NoteRepository
 
     @Before
     fun setUp() {
-        noteRepository = NoteRepositoryImpl()
+        noteRepository =
+            NoteRepositoryImpl(
+                euniceRemoteClient = euniceRemoteClient,
+            )
     }
 
     @Test
-    fun noteRepository_getNotes() {
-        noteRepository.getNotes().forEachIndexed { index, note ->
-            assertEquals("User $index", note.userName)
+    fun noteRepository_getNotes() =
+        runTest {
+            noteRepository.getNotes().forEach { note ->
+                assertEquals(true, note.content.isNotBlank())
+            }
         }
-    }
+
+    @Test
+    fun noteRepository_createNote() =
+        runTest {
+            noteRepository.createNote(
+                userName = "Eunice",
+                content = "Hello, World!",
+            )
+            assert(true)
+        }
 }
