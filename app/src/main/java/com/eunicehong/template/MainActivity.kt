de987fd71db7d22101f7a1cf1c6cdf6724c6860d
eunice-hong <dev@eunice-hong.com>
2024-11-22 14:09:41 +0900

    feat: move to NoteEditorActivity on click fab on MainActivity

--- app/src/main/java/com/eunicehong/template/MainActivity.kt
+++ app/src/main/java/com/eunicehong/template/MainActivity.kt
@@ -2,7 +2,6 @@ package com.eunicehong.template
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -16,22 +15,16 @@ import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarDuration
-import androidx.compose.material3.SnackbarHost
-import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import com.eunicehong.template.core.ui.note.NoteList
 import com.eunicehong.template.core.ui.theme.AndroidTemplateTheme
 import com.eunicehong.template.note.detail.NoteDetailActivity
+import com.eunicehong.template.note.edit.NoteEditorActivity
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -42,8 +35,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            val snackBarHostState = remember { SnackbarHostState() }
-            val coroutineScope = rememberCoroutineScope()
             AndroidTemplateTheme {
                 Scaffold(
                     modifier = Modifier.fillMaxSize(),
@@ -58,26 +49,8 @@ class MainActivity : ComponentActivity() {
                     floatingActionButton = {
                         FloatingActionButton(
                             onClick = {
-                                coroutineScope.launch {
-                                    // TODO(@eunice-hong): Open new note screen
-                                    val result =
-                                        snackBarHostState.showSnackbar(
-                                            "새로운 메모",
-                                            "확인",
-                                            withDismissAction = true, // 스낵바의 확인 버튼을 보여줄지 여부
-                                            SnackbarDuration.Short, // 스낵바 보여주는 시간
-                                        )
-
-                                    when (result) {
-                                        SnackbarResult.Dismissed -> {
-                                            Log.d("snackBar", "snackBar: 스낵바 닫아짐")
-                                        }
-
-                                        SnackbarResult.ActionPerformed -> {
-                                            Log.d("snackBar", "snackBar: 확인 버튼 눌러짐")
-                                        }
-                                    }
-                                }
+                                val editorIntent = Intent(baseContext, NoteEditorActivity::class.java)
+                                startActivity(editorIntent)
                             },
                         ) {
                             Icon(
@@ -85,12 +58,6 @@ class MainActivity : ComponentActivity() {
                                 contentDescription = "Add New Note",
                             )
                         }
-                    },
-                    snackbarHost = {
-                        // 스낵바가 보여지는 부분은 따로 지정해 주어야 함
-                        SnackbarHost(
-                            hostState = snackBarHostState,
-                        )
                     },
                 ) { innerPadding ->
                     val noteList = noteViewModel.noteList.collectAsState(initial = emptyList())
