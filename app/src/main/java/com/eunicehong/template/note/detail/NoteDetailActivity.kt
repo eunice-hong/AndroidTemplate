62c4cccfa6f2b99dde5f5d4710e1fd317d58bb7b
eunice-hong <dev@eunice-hong.com>
2024-11-22 13:38:47 +0900

    feat: show note details from NoteDetailViewModel

--- app/src/main/java/com/eunicehong/template/note/detail/NoteDetailActivity.kt
+++ app/src/main/java/com/eunicehong/template/note/detail/NoteDetailActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -15,23 +16,39 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.eunicehong.template.R
 import com.eunicehong.template.core.ui.theme.AndroidTemplateTheme
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 /**
  * 방명록 상세 화면입니다.
  */
 @AndroidEntryPoint
 class NoteDetailActivity : ComponentActivity() {
+    @Inject
+    lateinit var noteDetailViewModelFactory: NoteDetailViewModel.Factory
+
+    /**
+     * 방명록 상세 화면 뷰 모델입니다.
+     */
+    private val noteDetailViewModel by viewModels<NoteDetailViewModel> {
+        NoteDetailViewModel.provideFactory(
+            noteDetailViewModelFactory,
+            id = intent.getLongExtra("id", -1),
+        )
+    }
+
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val note = noteDetailViewModel.note.collectAsState(null).value
             AndroidTemplateTheme {
                 Scaffold(
                     modifier = Modifier.fillMaxSize(),
@@ -48,7 +65,7 @@ class NoteDetailActivity : ComponentActivity() {
                             },
                             title = {
                                 Text(
-                                    text = stringResource(R.string.note_detail_title).format(""),
+                                    text = stringResource(R.string.note_detail_title).format(note?.userName ?: ""),
                                 )
                             },
                             modifier = Modifier.fillMaxWidth(),
@@ -57,12 +74,13 @@ class NoteDetailActivity : ComponentActivity() {
                 ) { innerPadding ->
                     Text(
                         // TODO(@eunice-hong): 상세 화면 구현
-                        text = "상세 화면 구현",
+                        text = note?.content ?: "",
                         style = MaterialTheme.typography.bodyLarge,
+                        color = MaterialTheme.colorScheme.onBackground,
                         modifier =
-                            Modifier
-                                .fillMaxSize()
-                                .padding(innerPadding),
+                        Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
                     )
                 }
             }
