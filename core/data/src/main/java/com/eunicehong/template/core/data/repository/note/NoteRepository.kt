8eec8e8a650df19eadc4809108ca69d641ac97ec
eunice-hong <dev@eunice-hong.com>
2024-12-15 00:39:27 +0900

    feat: use Result on repository methods

--- core/data/src/main/java/com/eunicehong/template/core/data/repository/note/NoteRepository.kt
+++ core/data/src/main/java/com/eunicehong/template/core/data/repository/note/NoteRepository.kt
@@ -1,6 +1,8 @@
 package com.eunicehong.template.core.data.repository.note
 
 import com.eunicehong.template.core.model.note.Note
+import com.eunicehong.template.core.model.result.Result
+import kotlinx.coroutines.flow.Flow
 
 /**
  * 방명록 관련 데이터 처리
@@ -9,16 +11,16 @@ interface NoteRepository {
     /**
      * 방명록 목록을 가져옵니다.
      */
-    suspend fun getNotes(): List<Note>
+    fun getNotes(): Flow<Result<List<Note>>>
 
     /**
      * 방명록 상세 정보를 가져옵니다.
      */
-    suspend fun getNote(id: Long): Note
+    fun getNote(id: Long): Flow<Result<Note>>
 
-    suspend fun createNote(
+    fun createNote(
         creatorUid: String,
         userName: String,
         content: String,
-    )
+    ): Flow<Result<Note>>
 }
